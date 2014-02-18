d6b23c1728533a32a208e6c024158d9fd0fb07e9
cpfaff <ctpfaff@arcor.de>
2014-02-18 14:28:46 +0100

    Improvements on annotation tools

--- rtematres/R/rtematres.annotation.R
+++ rtematres/R/rtematres.annotation.R
@@ -1,22 +1,30 @@
 #' This is the annotation features provided by rtematres
 
-#' You can annotate and clean datasets base on a tematres 
+#' You can semantically annotate datasets base on a tematres 
 #' thesaurus. 
 
 base.find.common.concept <- function(input) {
-   if(is.list(input)){
-      maximum_length = max(sapply(input, function(x) length(x), USE.NAMES=F))
-      input = data.frame(sapply(input, function(x) c(x, rep(NA, maximum_length - length(x)))))
-   } 
-   for(i in 1:nrow(input)){
-      if(length(unique(as.character(unlist(input[i,])))) > 1) {
-	 return(unique(as.character(unlist(input[i-1,]))))
+   if(class(input) == "numeric") return("numeric")
+   if(class(input) %in% c("factor", "character")) {
+      unique_terms = as.character(unique(input))
+      broader_terms = sapply(unique_terms, function(x) rtematres(task="broaden", term = x)) 
+      if(class(broader_terms) == "list") {
+	 maxlen <- max(sapply(broader_terms,length))
+	 broader_terms = data.frame(lapply(broader_terms , function(x,len){ x[1:len] } , len=maxlen))
+      }
+      for(i in 1:nrow(broader_terms)){
+	 if(length(unique(as.character(unlist(broader_terms[i,])))) > 1) {
+	    decision = (unique(as.character(unlist(broader_terms[i-1,]))))
+	 }
+      }
+      return(decision[1])
    }
 }
-}
 
-#' @export find.common.concept
-
-find.common.concept <- function(input) {
-   if(identical(base.find.common.concept(input),character(0))) warning("Sorry no common concept found here! Check your column for consitency!") else return(base.find.common.concept(input))
+annotate.dataframe <- function(dataframe) {
+   concepts = lapply(dataframe, function(x) base.find.common.concept(x))
+   definitions = lapply(concepts, function(x) rtematres(task="define", term=x))
+   annotation = list(concecpts=concepts, 
+		     definitions=definitions)
+   return(do.call(cbind, annotation))
 }
