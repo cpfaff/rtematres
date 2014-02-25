109b842b6a60a63cc1c8e90d45fce8f29f5d2e54
cpfaff <ctpfaff@arcor.de>
2014-02-25 10:14:07 +0100

    This cleans up the api and adds tests
    o This closes #3 Clean up base API
    o This closes #4 Add Tests for all base api tasks
    o Also updates the manual

--- rtematres/R/rtematres.api.base.R
+++ rtematres/R/rtematres.api.base.R
@@ -1,22 +1,28 @@
-#' Basic tematres server api
-#' 
-#' Features the basic tasks of the tematres server api. With no sugar added. 
+#' Access to basic tematres server api
+#'
+#' Features the tasks of the tematres server api. With no sugar added.
 #' They are the basic building blocks for more convenient user functions.
 #'
-#' @param task The api task you like to execute. Use the the "availableTasks"
+#' @param task The api task you like to perform. Use the the task "availableTasks"
 #' 	  to get an overview about the base api. It returns a data frame with
 #'        descriptions and the arguments for the tasks.
 #'
 #' @param argument Is the argument for the api task. You find the information
-#'        about the arguments when you call the task "availableTasks".
+#'        about the type of arguments when you call the task "availableTasks".
+#'        It depends on the task if the argument is numeric or a character.
 #'
-#' @return The function returns either a dataframe for information or a list
-#'         of keywords and ids
+#' @return The function returns either a dataframe for "availableTasks" or a list
+#'         of information elements for a certain task.
+#'
 #' @examples \dontrun{
 #'     rtematres.api(task = "availableTasks")
 #'     rtematres.api(task = "fetchVocabularyData")
 #'     rtematres.api(task = "fetchTopTerms")
 #'     rtematres.api(task = "search", argument = "measurement")
+#'     rtematres.api(task = "fetch", argument = "measurement")
+#'     rtematres.api(task = "searchNotes", argument = "measurement")
+#'     rtematres.api(task = "suggest", argument = "measurement")
+#'     rtematres.api(task = "suggestDetails", argument = "measurement")
 #'     rtematres.api(task = "letter", argument = "t")
 #'     rtematres.api(task = "fetchTerm", argument = 12)
 #'     rtematres.api(task = "fetchDown", argument = 4 )
@@ -31,10 +37,7 @@
 #'     rtematres.api(task = "fetchSourceTerm", argument = "measurement")
 #'     rtematres.api(task = "fetchTerms", argument = '12,13' )
 #'     rtematres.api(task = "fetchRelatedTerms", argument = '12,13' )
-#'     rtematres.api(task = "fetchSimilar", argument = "mesur")
 #'     rtematres.api(task = "fetchLast")
-#'     rtematres.api.conversion.keyword_id(given = "measurement")
-#'     rtematres.api.conversion.keyword_id(given = 8)
 #'  }
 #'
 #' @import RCurl
@@ -53,7 +56,7 @@ rtematres.api <- function(task = "availableTasks", argument) {
 	}
 
 	# select task
-	task = match.arg(task, c("fetchVocabularyData", "suggest", "suggestDetails", "fetchTopTerms", "search", "fetch", "searchNotes", "fetchCode", "letter", "fetchTerm", "fetchAlt", "fetchDown", "fetchUp", "fetchRelated", "fetchNotes", "fetchDirectTerms", "fetchURI", "fetchTargetTerms", "fetchSourceTerms", "fetchTerms", "fetchRelatedTerms", "fetchLast", "fetchSimilar"))
+	task = match.arg(task, c("fetchVocabularyData", "suggest", "suggestDetails", "fetchTopTerms", "search", "fetch", "searchNotes", "fetchCode", "letter", "fetchTerm", "fetchAlt", "fetchDown", "fetchUp", "fetchRelated", "fetchNotes", "fetchDirectTerms", "fetchURI", "fetchTargetTerms", "fetchSourceTerms", "fetchTerms", "fetchRelatedTerms", "fetchLast"))
 
 	tasks_need_no_argument = c("fetchLast", "fetchTopTerms", "fetchVocabularyData")
 
@@ -90,66 +93,66 @@ rtematres.api <- function(task = "availableTasks", argument) {
 					publisher = "//publisher",
 					rights = "//rights",
 					count_of_terms = "//cant_terms",
-					status = "//status", 
+					status = "//status",
 					server_version = "//version",
 					api_version = "//web_service_version")
 
-	fetchTopTerms_list = list(term = "//term/string", 
+	fetchTopTerms_list = list(term = "//term/string",
 				  language = "//term/lang",
 				  term_id = "//term/term_id",
-				  code = "//term/code", 
+				  code = "//term/code",
 				  is.meta.term = "//term/isMetaTerm")
 
 	# used in search and search notes (searches complete strings)
 	# (usable with 2 letters or more)
 	# implement a "to short check"
-	search_list = list(term = "//term/string", 
-			   term_id = "//term/term_id", 
-			   is.meta.term = "//term/isMetaTerm", 
+	search_list = list(term = "//term/string",
+			   term_id = "//term/term_id",
+			   is.meta.term = "//term/isMetaTerm",
 			   no_term_string = "//term/no_term_string",
-			   index = "//term/index", 
+			   index = "//term/index",
 			   order = "//term/order")
 
 	# this task is for search with exact matching available sind api 1.4
-	fetch_list = list(term = "//term/string", 
-			  term_id = "//term/term_id", 
-			  is.meta.term = "//term/isMetaTerm", 
+	fetch_list = list(term = "//term/string",
+			  term_id = "//term/term_id",
+			  is.meta.term = "//term/isMetaTerm",
 			  no_term_string = "//term/no_term_string",
 			  order = "//term/order")
 
-	# also a search but only from start of string (usable with 2 letters or more) 
+	# also a search but only from start of string (usable with 2 letters or more)
 	# implement a "to short check"
-	suggest_list = list(term = "//result/term") 
+	suggest_list = list(term = "//result/term")
 
-	# like the suggest but with more details 
-	suggestDetails_list = list(term = "//term/string", 
-				   term_id = "//term/term_id", 
-				   is.meta.term = "//term/isMetaTerm", 
+	# like the suggest but with more details
+	suggestDetails_list = list(term = "//term/string",
+				   term_id = "//term/term_id",
+				   is.meta.term = "//term/isMetaTerm",
 				   no_term_string = "//term/no_term_string",
-				   index = "//term/index", 
+				   index = "//term/index",
 				   order = "//term/order")
-	
-	# a search for terms starting with a letter (only 1 letter) 
-	letter_list = list(term = "//term/string", 
-			   term_id = "//term/term_id", 
-			   is.meta.term = "//term/isMetaTerm", 
+
+	# a search for terms starting with a letter (only 1 letter)
+	letter_list = list(term = "//term/string",
+			   term_id = "//term/term_id",
+			   is.meta.term = "//term/isMetaTerm",
 			   no_term_string = "//term/no_term_string",
 			   relation_type_id = "//term/relation_type_id")
 
-	fetchTerm_list = list(term = "//term/string", 
+	fetchTerm_list = list(term = "//term/string",
 			      language = "//term/lang",
 			      term_id = "//term/term_id",
 			      tema_id = "//term/tema_id",
-			      code = "//term/code", 
+			      code = "//term/code",
 			      is.meta.term = "/term/isMetaTerm",
 			      created_at = "//term/date_create",
 			      last_modified_at = "//term/date_mod")
 
-	fetchTerms_list = list(term = "//term/string", 
+	fetchTerms_list = list(term = "//term/string",
 			       term_id = "//term/term_id",
 			       is.meta.term = "//term/isMetaTerm")
 
-	fetchAlt_list = list(term = "//term/string", 
+	fetchAlt_list = list(term = "//term/string",
 			     language = "//term/lang",
 			     term_id = "//term/term_id",
 			     code = "//term/code",
@@ -166,7 +169,7 @@ rtematres.api <- function(task = "availableTasks", argument) {
 			      relation_code = "//term/relation_code",
 			      relation_label = "//terms/relation_label",
 			      has_narrower_terms = "//term/hasMoreDown")
-	
+
 	fetchUp_list = list(term = "//term/string",
 			    term_id = "//term/term_id",
 			    is.meta.term = "//term/isMetaTerm",
@@ -183,7 +186,7 @@ rtematres.api <- function(task = "availableTasks", argument) {
 				 relation_label = "//terms/relation_label",
 				 relation_type_id = "//relation_type_id")
 
-	fetchNotes_list = list(term = "//term/string", 
+	fetchNotes_list = list(term = "//term/string",
 			       term_id = "//term/term_id",
 			       note_id = "//term/note_id",
 			       note_type = "//term/note_type",
@@ -204,33 +207,33 @@ rtematres.api <- function(task = "availableTasks", argument) {
 			     link = "//term/link")
 
 	# fetch target information
-	fetchTargetTerms_list = list(term = "//term/string", 
+	fetchTargetTerms_list = list(term = "//term/string",
 				     term_id = "//term/term_id",
-				     term_url = "//term/url", 
+				     term_url = "//term/url",
 				     term_uri = "//term/uri",
 				     target_vocabulary_label = "//term/target_vocabulary_label",
 				     target_vocabulary_tag = "//term/target_vocabulary_tag",
 				     target_vocabulary_title = "//term/target_vocabulary_title")
 
-	fetchRelatedTerms_list = list(term = "//term/string", 
+	fetchRelatedTerms_list = list(term = "//term/string",
 				      term_id = "//term/term_id",
 				      is.meta.term = "//term/isMetaTerm")
 
 	fetch_similar_list = list(term = "//result/string")
 
-	fetchTerm_list = list(term = "//term/string", 
+	fetchTerm_list = list(term = "//term/string",
 			      language = "//term/lang",
 			      term_id = "//term/term_id",
 			      tema_id = "//term/tema_id",
-			      code = "//term/code", 
+			      code = "//term/code",
 			      is.meta.term = "/term/isMetaTerm",
 			      created_at = "//term/date_create",
 			      last_modified_at = "//term/date_mod")
 
-	fetchLast_list = list(term = "//term/string", 
+	fetchLast_list = list(term = "//term/string",
 			      language = "//term/lang",
 			      term_id = "//term/term_id",
-			      code = "//term/code", 
+			      code = "//term/code",
 			      is.meta.term = "/term/isMetaTerm",
 			      created_at = "//term/date_create",
 			      last_modified_at = "//term/date_mod")
@@ -250,17 +253,15 @@ rtematres.api <- function(task = "availableTasks", argument) {
 		       fetchAlt = fetchAlt_list,
 		       fetchTerm = fetchTerm_list,
 		       fetchTerms = fetchTerms_list,
-		       fetchAlt = base_list,
 		       fetchDown = fetchDown_list,
 		       fetchUp = fetchUp_list,
 		       fetchRelated = fetchRelated_list,
 		       fetchRelatedTerms = fetchRelatedTerms_list,
 		       fetchNotes = fetchNotes_list,
 		       fetchDirectTerms = directTerms_list,
-		       fetchURI =fetchURI_list,
+		       fetchURI = fetchURI_list,
 		       fetchTargetTerms = fetchTargetTerms_list,
 		       fetchSourceTerms = base_list,
-		       fetchSimilar = fetch_similar_list,
 		       fetchLast = fetchLast_list
 		       )
 	if(is.list(sheme)) {
--- rtematres/tests/testthat/tematres.api.R
+++ rtematres/tests/testthat/tematres.api.R
@@ -1,112 +1,144 @@
 context("Test all base api tasks")
 
-# test_that("We are competible with API")
-# FIXME: Apis change so make the package work with a certain api version and warn if server and package might not be competible
-
 test_that("We can fetch the available tasks", {
 	expect_that(rtematres.api(task = "availableTasks"), is_a('data.frame'))
 })
 
 test_that("We can fetch information about the vocabulary", {
-	expect_that(rtematres.api(task = "fetchVocabularyData"), is_a('list')) 
+	expect_that(rtematres.api(task = "fetchVocabularyData"), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchVocabularyData")), equals(c("author", "title", "language", "uri", "contributor", "publisher", "rights", "creation_date", "last_modified", "count_terms", "status", "api_version")))
+	expect_that(names(rtematres.api(task = "fetchVocabularyData")), equals(c("vocabulary_id", "title", "author", "language", "scope", "keywords", "uri", "created_at", "last_modified_at", "contributors", "publisher", "rights", "count_of_terms", "status", "server_version", "api_version")))
 })
 
 test_that("We can fetch a list of last modified terms", {
-	expect_that(rtematres.api(task = "fetchLast"), is_a('list'))	 
+	expect_that(rtematres.api(task = "fetchLast"), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchLast")), equals('term'))	
+	expect_that(names(rtematres.api(task = "fetchLast")), equals(c("term", "language", "term_id", "code", "is.meta.term", "created_at", "last_modified_at")))
 })
 
 test_that("We can fetch alternative non preferred terms", {
 	expect_that(rtematres.api(task = "fetchAlt", argument = 1), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchAlt", argument = 1)), equals('term'))	
+	expect_that(names(rtematres.api(task = "fetchAlt", argument = 1)), equals(c("term", "language", "term_id", "code", "relation_type", "relation_code", "relation_label", "relation_type_id")))
 })
 
 test_that("We can fetch terms based on string matching", {
 	expect_that(rtematres.api(task = "suggest", argument = "a"), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "suggest", argument = "bio")), equals('term'))	
+	expect_that(names(rtematres.api(task = "suggest", argument = "bio")), equals('term'))
 })
 
 test_that("We can fetch alternative, related and direct hieraquical terms", {
 	expect_that(rtematres.api(task = "fetchDirectTerms", argument = 1), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchDirectTerms", argument = 1)), equals(c('term', 'relation')))	
+	expect_that(names(rtematres.api(task = "fetchDirectTerms", argument = 1)), equals(c("term", "language", "term_id", "is.meta.term", "code", "relation_type", "relation_code", "relation_label", "relation_type_id")))
 })
 
 test_that("We can fetch down the hirarchy", {
-	expect_that(rtematres.api(task = "fetchDown", argument = 1), is_a('list'))	        
+	expect_that(rtematres.api(task = "fetchDown", argument = 1), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchDown", argument = 1)), equals(c('term', 'id', 'relation_type', 'has_narrower_terms') ))	
+	expect_that(names(rtematres.api(task = "fetchDown", argument = 1)), equals(c("term", "language", "term_id", "is.meta.term", "relation_type", "relation_code", "relation_label", "has_narrower_terms")))
 })
 
 test_that("We can fetch up the hirarchy", {
-	expect_that(rtematres.api(task = "fetchUp", argument = 1), is_a('list'))	        
+	expect_that(rtematres.api(task = "fetchUp", argument = 1), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchUp", argument = 1)), equals(c('term', 'id', 'order')))	
+	expect_that(names(rtematres.api(task = "fetchUp", argument = 1)), equals(c("term", "term_id", "is.meta.term", "relation_type_id", "order")))
 })
 
 test_that("We can fetch notes or descriptions for a term", {
-	expect_that(rtematres.api(task = "fetchNotes", argument = 1), is_a('list')) 
+	expect_that(rtematres.api(task = "fetchNotes", argument = 1), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchNotes", argument = 1)), equals(c('id', 'term', 'language', 'description')))	
+	expect_that(names(rtematres.api(task = "fetchNotes", argument = 1)), equals(c("term", "term_id", "note_id", "note_type", "note_language", "note_text")))
 })
 
 test_that("We can fetch related terms for a id ", {
 	expect_that(rtematres.api(task = "fetchRelated", argument = 1), is_a('list'))
 	# and
-	expect_that(names(rtematres.api(task = "fetchRelated", argument = 1)), equals('term'))	
+	expect_that(names(rtematres.api(task = "fetchRelated", argument = 1)), equals(c("term", "language", "term_id", "is.meta.term", "code", "relation_type", "relation_code", "relation_label", "relation_type_id")))
 })
 
-# FIXME:
-# test_that("We can fetch related terms for multiple ids")
+test_that("We can fetch related terms for a id ", {
+	expect_that(rtematres.api(task = "fetchRelatedTerms", argument = 1), is_a('list'))
+	# and
+	expect_that(names(rtematres.api(task = "fetchRelatedTerms", argument = 1)), equals(c("term", "term_id", "is.meta.term")))
+})
 
-# NOTE: here the api is not very consistent as the name of the task is letter and the api field for
-# 	the task name says fetchSimilar
 test_that("We can fetch similar terms given a letter", {
 	  expect_that(rtematres.api(task = "letter", argument = 'a'), is_a('list'))
 	  # and
-	  expect_that(names(rtematres.api(task = "letter", argument = 'a')), equals('term'))	
+	  expect_that(names(rtematres.api(task = "letter", argument = 'a')), equals('term'))
 })
 
 test_that("We can retrieve data about target terms mapped via web services", {
 	  expect_that(rtematres.api(task = "fetchTargetTerms", argument = 1), is_a('list'))
 	  # and
-	  expect_that(names(rtematres.api(task = "fetchTargetTerms", argument = 1)), equals(c('term') ))	
+	  expect_that(names(rtematres.api(task = "fetchTargetTerms", argument = 1)), equals(c("term", "term_id", "term_url", "term_uri", "target_vocabulary_label", "target_vocabulary_tag", "target_vocabulary_title")))
 })
 
 test_that("We can retrieve simple term data", {
 	  expect_that(rtematres.api(task = "fetchTerm", argument = 1), is_a('list'))
 	  # and
-	  expect_that(names(rtematres.api(task = "fetchTerm", argument = 1)), equals(c('term', 'language', 'created_at', 'last_modified')))	
+	  expect_that(names(rtematres.api(task = "fetchTerm", argument = 1)), equals(c("term", "language", "term_id", "tema_id", "code", "is.meta.term", "created_at", "last_modified_at")))
 })
 
-# FIXME: This is useless as it is not returning anything but the terms. It is not the fetchTerm function 
-#        for multiple ids. 
-# test_that("We can retrieve simple term data for comma separated IDs", {
-	  # expect_that(rtematres.api(task = "fetchTerms", argument = "1,2"), is_a('list'))
-	  # # and
-	  # expect_that(names(rtematres.api(task = "fetchTerms", argument = 1)), equals(c('term', 'language', 'created_at', 'last_modified')))	
-# })
+test_that("We can retrieve simple term data for comma separated IDs", {
+	  expect_that(rtematres.api(task = "fetchTerms", argument = "1,2"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "fetchTerms", argument = 1)), equals(c("term", "term_id", "is.meta.term")))
+})
 
 test_that("We can retrieve top terms", {
 	  expect_that(rtematres.api(task = "fetchTopTerms"), is_a('list'))
 	  # and
-	  expect_that(names(rtematres.api(task = "fetchTopTerms")), equals(c('term', 'language')))	
+	  expect_that(names(rtematres.api(task = "fetchTopTerms")), equals(c("term", "language", "term_id", "code", "is.meta.term")))
 })
 
-# rtematres.api.conversion.term_id(given = "concept")
+# fixme: I do not understand the task atm so it is using the base list
+test_that("We can retrieve terms by code", {
+	  expect_that(rtematres.api(task = "fetchCode", argument = "19.11.1981"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "fetchCode", argument = "19.11.1981")), equals(c("term")))
+})
 
-      # tasks_available                                                                     tasks_description                                    tasks_argument
-            # fetchURI         Search and retrieve data about URI or internet entities linked to one term_id                              one ID term_id (int)
-              # search                                                             Search and retrieve terms                         search expresion (string)
-         # searchNotes                                                     Retrieve terms searching in notes                         search expresion (string)
-             # suggest                  Simple search and retrieve terms who start with string (only string)                         search expresion (string)
-      # suggestDetails p
+test_that("We can search for terms", {
+	  expect_that(rtematres.api(task = "search", argument = "plant organ"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "search", argument = "plant organ")), equals(c("term", "term_id", "is.meta.term", "no_term_string", "index", "order")))
+})
 
+test_that("We can search exact match of term", {
+	  expect_that(rtematres.api(task = "fetch", argument = "plant organ"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "fetch", argument = "plant organ")), equals(c("term", "term_id", "is.meta.term", "no_term_string", "order")))
+})
 
-# rtematres.api(task = "fetchCode" argument = "bio") # FIXME: I do not understand this one
-# fetchCode                                                     Retrieve simple term data by code                                     code (string)
+test_that("We can search in notes", {
+	  expect_that(rtematres.api(task = "searchNotes", argument = "plant organ"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "searchNotes", argument = "plant organ")), equals(c("term", "term_id", "is.meta.term", "no_term_string", "index", "order")))
+})
+
+test_that("We can search terms starting with the same letter", {
+	  expect_that(rtematres.api(task = "suggest", argument = "a"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "suggest", argument = "a")), equals(c("term")))
+})
+
+test_that("We can search details of a term", {
+	  expect_that(rtematres.api(task = "suggestDetails", argument = "plant organ"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "suggestDetails", argument = "plant organ")), equals(c("term", "term_id", "is.meta.term", "no_term_string", "index", "order")))
+})
+
+test_that("We can fetch URI information for a term", {
+	  expect_that(rtematres.api(task = "fetchURI", argument = "plant organ"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "fetchURI", argument = "plant organ")), equals(c("link_type", "link")))
+})
+
+test_that("We can fetch URI information for a term", {
+	  expect_that(rtematres.api(task = "fetchSourceTerms", argument = "plant organ"), is_a('list'))
+	  # and
+	  expect_that(names(rtematres.api(task = "fetchSourceTerms", argument = "plant organ")), equals(c("term")))
+})
