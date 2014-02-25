5bbc03b8f18805f3114f499fd622005b98243c6a
cpfaff <ctpfaff@arcor.de>
2014-02-25 11:51:44 +0100

    Improves the date helpers

--- rtematres/R/rtematres.helpers.R
+++ rtematres/R/rtematres.helpers.R
@@ -53,45 +53,50 @@ are.we.competible <- function(package_api_version = rtematres.options("tematres_
   }
 }
 
-as.IsoDate <- function(input){
-	if(class(input) != "character") input = as.character(input)
-	input = empty.to.na(input)
-	return(input)
-	if(length(input) == 1) {
-	  dmy = FALSE
-	  mdy = FALSE
+as.IsoDate <- function(input, return_format = F){
+  if(class(input) != "character") input = as.character(input)
+  input = string.empty.as_na(input)
+  if(length(input) == 1) {
+    dmy = FALSE
+    mdy = FALSE
+    string = NA
 
-	  # matches dd/mm/yyyy or dd.mm.yyyy or dd-mm-yyyy
-	  if(grepl("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$", input)) {
-		  dmy = TRUE
-		  if(grepl("/", input)) clean_dmy = strsplit(input, split = "/")
-		  if(grepl("\\.", input)) clean_dmy = strsplit(input, split = ".")
-		  if(grepl("-", input)) clean_dmy = strsplit(input, split = "-")
-		  string = paste(unlist(clean_dmy)[c(3,2,1)], collapse = "-")
-	  }
+    # matches dd/mm/yyyy or dd.mm.yyyy or dd-mm-yyyy
+    if(grepl("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$", input)) {
+      dmy = TRUE
+      if(grepl("/", input)) clean_dmy = strsplit(input, split = "/")
+      if(grepl("\\.", input)) clean_dmy = strsplit(input, split = "\\.")
+      if(grepl("-", input)) clean_dmy = strsplit(input, split = "-")
+      string = paste(unlist(clean_dmy)[c(3,2,1)], collapse = "-")
+    }
 
-	  # matches mm/dd/yyyy or mm.dd.yyyy or mm-dd-yyyy
-	  if(grepl("^(?:(?:(?:0?[13578]|1[02])(\\/|-|\\.)31)\\1|(?:(?:0?[1,3-9]|1[0-2])(\\/|-|\\.)(?:29|30)\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:0?2(\\/|-|\\.)29\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\\/|-|\\.)(?:0?[1-9]|1\\d|2[0-8])\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$", input)) {
-		  mdy = TRUE
-		  if(grepl("/", input)) clean_mdy = strsplit(input, split = "/")
-		  if(grepl("\\.", input)) clean_mdy = strsplit(input, split = ".")
-		  if(grepl("-", input)) clean_mdy = strsplit(input, split = "-")
-		  string = paste(unlist(clean_mdy)[c(3,1,2)], collapse = "-")
-	  }
+    # matches mm/dd/yyyy or mm.dd.yyyy or mm-dd-yyyy
+    if(grepl("^(?:(?:(?:0?[13578]|1[02])(\\/|-|\\.)31)\\1|(?:(?:0?[1,3-9]|1[0-2])(\\/|-|\\.)(?:29|30)\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:0?2(\\/|-|\\.)29\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\\/|-|\\.)(?:0?[1-9]|1\\d|2[0-8])\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$", input)) {
+      mdy = TRUE
+      if(grepl("/", input)) clean_mdy = strsplit(input, split = "/")
+      if(grepl("\\.", input)) clean_mdy = strsplit(input, split = "\\.")
+      if(grepl("-", input)) clean_mdy = strsplit(input, split = "-")
+      string = paste(unlist(clean_mdy)[c(3,1,2)], collapse = "-")
+    }
 
-	  if(mdy == T && dmy == T) return(NA)
+    if(mdy == T && dmy == T) return(NA)
+    if(mdy == T && return_format == T) return("mdy")
+    if(dmy == T && return_format == T) return("dmy")
 
-	  if(grepl("^(\\d{4})\\D?(0[1-9]|1[0-2])\\D?([12]\\d|0[1-9]|3[01])$", input)) {
-		  if(grepl("/", input)) clean = strsplit(input, split = "/")
-		  if(grepl("\\.", input)) clean = strsplit(input, split = ".")
-		  if(grepl("-", input)) clean = strsplit(input, split = "-")
-		  string = paste(unlist(clean), collapse = "-")
-	  }
+    if(grepl("^(\\d{4})\\D?(0[1-9]|1[0-2])\\D?([12]\\d|0[1-9]|3[01])$", input)) {
+      if(return_format){
+	return("ymd")
+      }
+      if(grepl("/", input)) clean = strsplit(input, split = "/")
+      if(grepl("\\.", input)) clean = strsplit(input, split = "\\.")
+      if(grepl("-", input)) clean = strsplit(input, split = "-")
+      string = paste(unlist(clean), collapse = "-")
+    }
 
-	  return(string)
-	} else {
-	  unlist(lapply(input, function(x) as.IsoDate(x)))
-	}
+    return(string)
+  } else {
+    unlist(lapply(input, function(x) as.IsoDate(x, return_format = return_format)))
+  }
 }
 
 is.IsoDate <- function(input) {
@@ -107,21 +112,37 @@ is.IsoDate <- function(input) {
   }
 }
 
-# converts empty strings in a vector to NA
-empty.to.na <- function(input) {
-  if(class(input) != "character") input = as.character(input)
-  input[input == ""] <- NA
-  return(input)
+# check for valid years
+is.year <- function(input) {
+	if(any(is.na(input))) warning("There is a few values missing in your input. I ignore them!")
+	grepl("^[1-9][0-9]{3}$", input[!is.na(input)])
 }
 
-# check for valid years
-# is.year <- function(input) {
-	# if(any(is.na(input)) warning("There is a few values missing in your input. I ignore them!")
-	# grepl("^[1-9][0-9]{3}$", theyears[!is.na(theyears)])
-# }
+year.to.range <- function(input){
+	if(class(input) != "character") input = as.character(input)
+	is.year(input)
+	range <- max(input) - min(input)
+	if(range == 0) return (unique(input)) else range
+}
 
 # check for valid years
 # is.month <- function(input) {
 	# if(any(is.na(input)) warning("There is a few values missing in your input. I ignore them!")
 	# grepl("^?()[1-12]$", months)
 # }
+
+
+## String helpers
+
+# converts empty strings in a vector to NA
+string.empty.as_na <- function(input) {
+  if(class(input) != "character") input = as.character(input)
+  input[input == ""] <- NA
+  return(input)
+}
+
+string.empty.rm <- function(input){
+  if(class(input) != "character") input = as.character(input)
+  input[input != ""]
+}
+
