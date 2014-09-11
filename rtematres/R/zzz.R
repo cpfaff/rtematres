372aca044debc0b99a53b6f44a0279dff3dcb9cb
cpfaff <ctpfaff@arcor.de>
2014-09-11 15:21:51 +0200

    Removed the URL from options as this is not used

--- rtematres/R/zzz.R
+++ rtematres/R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad = function(lib, pkg) {
   .rtematres.env$.rtematres.opts = list(
-    tematres_url = "http://tematres.befdata.biow.uni-leipzig.de/vocab/index.php",
     tematres_service_url = "http://tematres.befdata.biow.uni-leipzig.de/vocab/services.php",
     package_api_version = "1.4",
     server_api_version = tryCatch(rtematres(task="fetchVocabularyData"), error = function(e) {return(NA)})
